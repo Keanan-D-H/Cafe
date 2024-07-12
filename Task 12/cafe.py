d5f98e3ef06fc520b370656674e3ddaa2bc5e56a
Keanan-D-H <Keananhinchliffe@outlook.com>
2024-07-12 10:57:26 +0100

    cafe.py added

---
+++ Task 12/cafe.py
@@ -0,0 +1,102 @@
+#***cafe***
+
+# Function Definitions
+
+# check_yes_no function checks whether an input is either 'yes' or 'no'
+# The function will repeatedly ask for an input until it is either 'yes'
+# or 'no'
+# The parameter 'answer' is a string
+# returns 'answer' (either yes or no)
+def check_yes_no(answer):
+    while answer !='yes' and answer != 'no':
+        answer = input ('Please only put in \'yes\' or \'no\'    ')
+    return answer
+
+# check_number function tries to turn 'item' to a float, if it can not
+# it will ask for a new input until a number is inputted
+# the 'item' parameter is a string
+# returns float(item)
+def check_number(item): 
+    while type(item) != float:
+        try:
+            item = float(item)
+        except:  
+            item = input("That is not a number, please input again.   ")
+    return item
+
+
+# check_repeat function checks if the user has inputted a value previously
+# if they have, the program will ask them if they want to overwrite the
+# previously input data 
+# item is a string and dictionary is a dictionary string would be stored in
+# returns 'yes' if they want to overwrite, no if they don't and 'no repeat'
+# if the item wasn't in the dictionary to begin with
+def check_repeat (item, dictionary):
+    if dictionary.get(item) != None:
+        repeat = input(f"""Unfortunately, {item} is already in our recorded 
+list.If we carry on with this item our previous record
+will be overwritten. Would you like to carry on?    """)
+        check_yes_no(repeat)
+        
+        return repeat
+    else:
+        return "no repeat"
+    
+
+
+# Variable Declarations
+menu = []
+stock = {}
+price = {}
+total_stock = 0
+
+#asking the user if they want to input data
+finished = input("Do you have any menu items to input?    ")
+finished = check_yes_no(finished)
+
+# While there are still menu items to be inputted, ask for the item name
+# price and how much the user has in stock
+while finished == "yes":
+    # ask for the name of the stock item
+    menu_item = input("What is the name of the menu item?    ")
+    menu_item_check = check_repeat(menu_item,stock)
+
+    # if the user has repeated a stock item name and does not want to
+    # overwrite the data then move on to next iteration
+    if menu_item_check == 'no':
+        finished = input("Do you have more menu items to input?    ")
+        finished = check_yes_no(finished)
+        continue
+
+    # if the user wants to overwrite previous data the stock_amount *
+    # price_input from data with the same name is removed from total_stock
+    elif menu_item_check == "yes":
+        total_stock -= stock.get(menu_item) * stock.get(menu_item)
+
+
+    stock_amount = input("How much do you have in stock:    ")
+    stock_amount = check_number(stock_amount)
+
+    price_input = input("How much does it cost (in £)?     ")
+    price_input = check_number(price_input)
+
+    #add menu_item to menu, {menu_item:stock_amount} to stock and {menu_item
+    # , price_input}
+    menu.append(menu_item)
+    stock[menu_item] = stock_amount
+    price[menu_item] = price_input
+    total_stock  += stock_amount * price_input
+
+
+
+    finished = input("Do you have more menu items to input?    ")
+    finished = check_yes_no(finished)
+
+
+print(stock)
+print(price)
+print(f"The total stock value is {total_stock}")
+
+
+
+
