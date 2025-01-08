5b8ec27e916665f9d38e88a2cdf2b885b444058d
Keanan Hinchliffe <Keananhinchliffe@outlook.com>
2025-01-08 14:53:13 +0000

    Presented the inputted data as a table
    The inputted data will now be presented to the user as a table rather than as raw data. If the user does not input data then no table shall be presented.

--- Task 12/cafe.py
+++ Task 12/cafe.py
@@ -1,7 +1,9 @@
 #***cafe***
+# Keanan Hinchliffe
+# A program to add up the total stock value for a cafe. 
+
 
 # Function Definitions
-
 
 def check_yes_no(answer):
     """
@@ -40,7 +42,6 @@ def check_number(item):
     return item
 
 
-
 def check_repeat (item, dictionary):
     """
     check_repeat function checks if the user has inputted a value previously
@@ -68,7 +69,6 @@ will be overwritten. Would you like to carry on?    """)
         return "no repeat"
     
 
-
 # Variable Declarations
 menu = []
 stock = {}
@@ -76,52 +76,62 @@ price = {}
 total_stock = 0
 
 #asking the user if they want to input data
-finished = input("Do you have any menu items to input?    ")
+finished = input("\nDo you have any menu items to input?    ")
 finished = check_yes_no(finished)
 
 # While there are still menu items to be inputted, ask for the item name
 # price and how much the user has in stock
 while finished == "yes":
     # ask for the name of the stock item
-    menu_entry = input("What is the name of the menu item?    ")
-    menu_entry_check = check_repeat(menu_entry,stock)
+    menu_entry = input("\nWhat is the name of the menu item?    ")
+    menu_entry_check = check_repeat(menu_entry.lower(),stock)
 
-    # if the user has repeated a stock item name and does not want to
-    # overwrite the data then move on to next iteration
+    # If the user has repeated a stock item name and does not want to
+    # overwrite the data then move on to next iteration.
     if menu_entry_check == 'no':
-        finished = input("Do you have more menu items to input?    ")
+        finished = input("\nDo you have more menu items to input?    ")
         finished = check_yes_no(finished)
         continue
 
-    # if the user wants to overwrite previous data the stock_amount *
-    # price_input from data with the same name is removed from total_stock
+    # If the user wants to overwrite previous data the stock_amount *
+    # price_input from data with the same name is removed from total_stock.
     elif menu_entry_check == "yes":
         total_stock -= stock.get(menu_entry) * stock.get(menu_entry)
 
 
-    stock_amount = input("How much do you have in stock:    ")
+    stock_amount = input("\nHow much do you have in stock:    ")
     stock_amount = check_number(stock_amount)
 
-    price_input = input("How much does it cost (in £)?     ")
+    price_input = input("\nHow much does it cost? (£)     ")
     price_input = check_number(price_input)
 
-    #add menu_entry to menu, {menu_entry:stock_amount} to stock and {menu_entry
-    # , price_input}
-    menu.append(menu_entry)
+    # Add menu_entry to menu, {menu_entry:stock_amount} to stock and 
+    # {menu_entry, price_input}
+    menu.append(menu_entry.lower())
     stock[menu_entry] = stock_amount
     price[menu_entry] = price_input
     total_stock  += stock_amount * price_input
 
-
-
-    finished = input("Do you have more menu items to input?    ")
+    finished = input("\n\nDo you have more menu items to input?    ")
     finished = check_yes_no(finished)
 
 
-print(stock)
-print(price)
-print(f"The total stock value is {total_stock}")
+if len(stock) != 0 :
+    # If stock has been inputted by the user present the inputted items 
+    # to the user as a table.
+
+    print()
+    print('{:^20}|{:^20}|{:^20}'.format("Item name", "Quantity", "Cost (£)"))  
+    dashes = '-' * 62                                        
+    print(dashes)
+
+    for key in stock:
+        # For each menu item print the items name, stock quantity and cost.
+        print('{:^20}|{:^20}|{:^20}'.format(key, stock[key], price[key]))
+
+    print(f"\nThe total stock value is £{"%.2f" % total_stock}.")
 
 
+print("\n\nGoodbye!")
 
 
